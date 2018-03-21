800cb8c0bbb38ca58cfad3137acf948018834cf8
marcelo corpucci <mcorpucci@gmail.com>
2018-03-20 21:45:18 -0700

    ejercicio resuelto usando teardown por api para recursos de entry y user.

--- src/test/java/com/effectivetesting/entry/TestCreateEntry.java
+++ src/test/java/com/effectivetesting/entry/TestCreateEntry.java
@@ -1,19 +1,16 @@
 package com.effectivetesting.entry;
 
-import static io.restassured.RestAssured.*;
-import static org.junit.Assert.*;
+import static io.restassured.RestAssured.delete;
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertTrue;
 
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.effectivetesting.entities.User;
 import com.effectivetesting.helpers.UserHelper;
@@ -57,21 +54,20 @@ public class TestCreateEntry {
 		
 		assertTrue(currentMessage.contains("Entry 'My newest post' created successfully."));
 	}
-	
 
 	@After
 	public void teardDown() {
-		driver.get("http://localhost:5000/admin/entry/");
-		driver.findElement(By.xpath("/html/body/div/table/tbody/tr/td[2]/form")).click();
-		
-	    WebDriverWait wait = new WebDriverWait(driver, 10);
-	    Alert alert = wait.until(ExpectedConditions.alertIsPresent());
-		   
-	    alert.accept();
-	    
-	    WebDriverWait waitForMessage = new WebDriverWait(driver, 10);
-	    waitForMessage.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div/div[2]")));
-	    
+		int entryID = given()
+    		.contentType("application/json")
+    	
+	    .when()
+			.get(DEFAULT_BASE_URL + "/entry")
+	    	
+		.then()
+        	.extract()
+			.path("num_results");
+
+		delete(DEFAULT_BASE_URL + "/entry/" + entryID);
 		delete(DEFAULT_BASE_URL + "/user/" + userID);
 		
 	    driver.quit();
