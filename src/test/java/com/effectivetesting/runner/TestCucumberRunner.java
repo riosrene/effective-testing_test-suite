fd21ce18b60b59a2c6ede941762cdf2b35a17cc0
corpucci.marcelo <corpucci.marcelo@gmail.com>
2018-03-26 20:49:30 -0300

    Se renombro el runner de cucumber con prefijo Test para que sea visible por maven.

--- src/test/java/com/effectivetesting/runner/TestCucumberRunner.java
+++ src/test/java/com/effectivetesting/runner/TestCucumberRunner.java
@@ -7,6 +7,6 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources", glue = "com.effectivetesting.gluecode", tags = {"@wip"}, plugin = {"json:target/cucumber.json" })
-public class CucumberRunner {
+public class TestCucumberRunner {
 
 }
