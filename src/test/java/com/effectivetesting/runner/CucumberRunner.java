23f3ef20ef502e774c5c4289a72a0830e80b8855
corpucci.marcelo <corpucci.marcelo@gmail.com>
2018-03-26 13:50:29 -0300

    Se agregaron modificaciones a cucumber options y un plgunin del pom para cucumber reports.

--- src/test/java/com/effectivetesting/runner/CucumberRunner.java
+++ src/test/java/com/effectivetesting/runner/CucumberRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources", glue = "com.effectivetesting.gluecode", tags = {"@wip"}, plugin = {"html:target/cucumber-html-report", "json:target/cucumber-json-report.json" })
+@CucumberOptions(features = "src/test/resources", glue = "com.effectivetesting.gluecode", tags = {"@wip"}, plugin = {"html:target/cucumber-html-report", "json:target/cucumber.json" })
 public class CucumberRunner {
 
 }
