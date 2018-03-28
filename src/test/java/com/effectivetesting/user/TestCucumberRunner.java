bdc0aed75ee1d307d5eeb785abcd27015f21c209
marcelo corpucci <mcorpucci@gmail.com>
2018-03-27 21:40:24 -0700

    Se agrego cucumberOptions para cucumber.json.

--- src/test/java/com/effectivetesting/user/TestCucumberRunner.java
+++ src/test/java/com/effectivetesting/user/TestCucumberRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources", glue = "com.effectivetesting.user")
+@CucumberOptions(features = "src/test/resources", glue = "com.effectivetesting.user", plugin = {"json:target/cucumber.json" })
 public class TestCucumberRunner {
 
 }
