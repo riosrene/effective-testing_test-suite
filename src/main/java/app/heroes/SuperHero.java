25388b3458dc9ef13e3782840088d72be67911c4
corpucci.marcelo <corpucci.marcelo@gmail.com>
2018-07-16 17:55:54 -0300

    definicion objetos.

--- src/main/java/app/heroes/SuperHero.java
+++ src/main/java/app/heroes/SuperHero.java
@@ -2,6 +2,6 @@ package app.heroes;
 
 public class SuperHero {
 	public String name = "Superman";
-	public int BattlesWon = 123;
+	public int battlesWon = 123;
 	public boolean isMetaHuman = true;
 }
