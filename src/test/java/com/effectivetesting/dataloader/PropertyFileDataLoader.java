1791f52ac9e831cb408ed8f0dc16391996e7beff
corpucci.marcelo <corpucci.marcelo@gmail.com>
2018-12-15 11:17:56 -0300

    Added property files to parametrize tests. Pending to refactor a command pattern.

---
+++ src/test/java/com/effectivetesting/dataloader/PropertyFileDataLoader.java
@@ -0,0 +1,39 @@
+package com.effectivetesting.dataloader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+public class PropertyFileDataLoader {
+
+	public Map<String, String> loadData(List<String> params) {
+		Properties propertyFile = new Properties();
+		InputStream input = null;
+		Map<String, String> data = new HashMap<String, String>();
+
+		try {
+			input = getClass().getClassLoader().getResourceAsStream(params.get(0));
+			propertyFile.load(input);
+			
+			for(int i=1; i < params.size(); i++) {
+				data.put(params.get(i), propertyFile.getProperty(params.get(i)));
+			}
+			
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return data;
+
+	}
+}
--- src/test/java/com/effectivetesting/entry/TestCreateEntry.java
+++ src/test/java/com/effectivetesting/entry/TestCreateEntry.java
@@ -2,6 +2,9 @@ package com.effectivetesting.entry;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
@@ -14,9 +17,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.effectivetesting.dataloader.PropertyFileDataLoader;
 import com.effectivetesting.pageobject.LoginPageObject;
 
 public class TestCreateEntry {
+	private PropertyFileDataLoader dataLoader;
+	private Map<String, String> testData;
+	private Map<String, String> testCredentials;
 	private WebDriver driver;
 	private LoginPageObject loginPage;
 	
@@ -24,17 +31,33 @@ public class TestCreateEntry {
 	public void postIsSuccessfull() {
 		loginPage = new LoginPageObject(driver);
 		String currentMessage = loginPage
-				.login("admin1@gmail.com", "admin1")
+				.login(testCredentials.get("email"), testCredentials.get("pass"))
 				.goToCreateEntry()
-				.createEntry("My newest post", "This is a post.")
+				.createEntry(testData.get("name"),testData.get("body"))
 				.getResultMessage();
 		
-		assertTrue(currentMessage.contains("Entry 'My newest post' created successfully."));
+		assertTrue(currentMessage.contains("Entry '" + testData.get("name") + "' created successfully."));
 	}
 	
 	@Before
 	public void setUp() {
-		System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver\\chromedriver.exe");
+		dataLoader = new PropertyFileDataLoader();
+		
+		List<String> credentialParams = new ArrayList<String>();
+		credentialParams.add("admin_credentials.properties");
+		credentialParams.add("email");
+		credentialParams.add("pass");
+		
+		List<String> testParams = new ArrayList<String>();
+		testParams.add("create_entry.properties");
+		testParams.add("name");
+		testParams.add("body");
+		
+		testCredentials = dataLoader.loadData(credentialParams);
+		testData = dataLoader.loadData(testParams);
+		
+//		System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "/opt/chromedriver/chromedriver");
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		driver.get("localhost:5000");
@@ -43,7 +66,8 @@ public class TestCreateEntry {
 	@After
 	public void teardDown() {
 		driver.get("http://localhost:5000/admin/entry/");
-		driver.findElement(By.xpath("/html/body/div/table/tbody/tr/td[2]/form")).click();
+		driver.findElement(By.xpath("/html/body/div/table/thead/tr/th[8]/a")).click();
+		driver.findElement(By.xpath("/html/body/div/table/tbody/tr[1]/td[2]/form/button")).click();
 		
 	    WebDriverWait wait = new WebDriverWait(driver, 10);
 	    Alert alert = wait.until(ExpectedConditions.alertIsPresent());
