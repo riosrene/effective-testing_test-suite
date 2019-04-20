e6830c1aa7f86fbfe26856884520d0c9786ea70f
Marcelo Corpucci <mcorpucci@gmail.com>
2019-04-20 11:10:55 -0300

    added fuelDosis, amounts changed.

--- src/main/java/virtualecu/core/FuelDosis.java
+++ src/main/java/virtualecu/core/FuelDosis.java
@@ -2,7 +2,7 @@ package virtualecu.core;
 
 public class FuelDosis {
 	public static final float STAGE_O = 1.5f;
-	public static final float STAGE_1 = 1.5f;
-	public static final float STAGE_2 = 1.5f;
-	public static final float STAGE_3 = 1.5f;
+	public static final float STAGE_1 = 2.0f;
+	public static final float STAGE_2 = 2.5f;
+	public static final float STAGE_3 = 3.0f;
 }
